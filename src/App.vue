0e595ea230fc238208bccae44928781c3c6a9e04
Ruslan Igorevich <ruslan.igorevich@list.ru>
2019-05-14 10:50:09 +0300

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,22 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
+    <Header/>
     <v-content>
       <HelloWorld/>
-      <Footer/>
     </v-content>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
+import Header from './components/Header'
 import HelloWorld from './components/HelloWorld'
 import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
+    Header,
     HelloWorld,
     Footer
   },
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-toolbar dark color="teal lighten-3">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+
+    <v-toolbar-title class="white--text">Title</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>apps</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>refresh</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>more_vert</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    data () {
+        return {
+        //
+        }
+    }
+  }
+</script>
+
+<style>
+
+</style>
