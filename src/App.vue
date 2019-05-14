d6501a2cf995f2d8534aae2913ab895113e12e02
Ruslan Igorevich <ruslan.igorevich@list.ru>
2019-05-14 12:51:12 +0300

    add table

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,30 @@
     <Header/>
     <v-content>
       <Grid/>
+      <Table/>
     </v-content>
     <Footer/>
   </v-app>
 </template>
 
 <script>
-import Header from './components/Header'
-import Grid from './components/Grid'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Grid from "./components/Grid";
+import Table from "./components/Table";
+import Footer from "./components/Footer";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     Grid,
+    Table,
     Footer
   },
-  data () {
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
