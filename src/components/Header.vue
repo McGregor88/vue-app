580ab002f316d52a0ea1b69626baf57c7b8e3749
Ruslan Igorevich <ruslan.igorevich@list.ru>
2019-05-16 12:48:26 +0300

    add search in table

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,39 +1,97 @@
 <template>
-  <v-toolbar dark color="teal lighten-3">
-    <v-toolbar-side-icon></v-toolbar-side-icon>
+  <div class="header">
+    <v-toolbar dark color="teal lighten-3">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
 
-    <v-toolbar-title class="white--text">Title</v-toolbar-title>
+      <v-toolbar-title class="white--text">Title</v-toolbar-title>
 
-    <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat to="/">Home</v-btn>
-      <v-btn flat to="/about">About</v-btn>
-    </v-toolbar-items>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat to="/">Home</v-btn>
+        <v-btn flat to="/about">About</v-btn>
+      </v-toolbar-items>
 
-    <v-btn icon>
-      <v-icon>search</v-icon>
-    </v-btn>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
 
-    <v-btn icon>
-      <v-icon>apps</v-icon>
-    </v-btn>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
 
-    <v-btn icon>
-      <v-icon>refresh</v-icon>
-    </v-btn>
+      <v-btn icon>
+        <v-icon>refresh</v-icon>
+      </v-btn>
 
-    <v-btn icon>
-      <v-icon>more_vert</v-icon>
-    </v-btn>
-  </v-toolbar>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-layout wrap>
+      <v-navigation-drawer v-model="drawer" :mini-variant="mini" absolute dark temporary>
+        <v-list class="pa-1">
+          <v-list-tile v-if="mini" @click.stop="mini = !mini">
+            <v-list-tile-action>
+              <v-icon>chevron_right</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+
+          <v-list-tile avatar tag="div">
+            <v-list-tile-avatar>
+              <img src="https://randomuser.me/api/portraits/men/85.jpg">
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title>John Leider</v-list-tile-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-btn icon @click.stop="mini = !mini">
+                <v-icon>chevron_left</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+
+        <v-list class="pt-0" dense>
+          <v-divider light></v-divider>
+
+          <v-list-tile v-for="item in items" :key="item.title" :to="item.url">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+    </v-layout>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      //
+      drawer: null,
+      items: [
+        {
+          url: "/",
+          title: "Home",
+          icon: "dashboard"
+        },
+        {
+          url: "/about",
+          title: "About",
+          icon: "question_answer"
+        }
+      ],
+      mini: false,
+      right: null
     };
   }
 };
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,12 +1,20 @@
 <template>
   <v-container>
     <v-toolbar flat color="white">
-      <v-toolbar-title>My CRUD</v-toolbar-title>
+      <v-toolbar-title>Product table</v-toolbar-title>
       <v-divider class="mx-2" inset vertical></v-divider>
       <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+        class="mb-2 mr-2"
+      ></v-text-field>
       <v-dialog v-model="dialog" max-width="500px">
         <template v-slot:activator="{ on }">
-          <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+          <v-btn color="success" dark class="mb-2" v-on="on">Add product</v-btn>
         </template>
         <v-card>
           <v-card-title>
@@ -43,7 +51,7 @@
         </v-card>
       </v-dialog>
     </v-toolbar>
-    <v-data-table :headers="headers" :items="desserts" class="elevation-1">
+    <v-data-table :headers="headers" :items="desserts" :search="search" class="elevation-1">
       <template v-slot:items="props">
         <td>{{ props.item.name }}</td>
         <td class="text-xs-right">{{ props.item.calories }}</td>
@@ -66,6 +74,7 @@
 export default {
   data: () => ({
     dialog: false,
+    search: "",
     headers: [
       {
         text: "Dessert (100g serving)",
