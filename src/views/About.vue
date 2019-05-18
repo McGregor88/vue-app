13d0303782a705c9a71275bc5bca6ee6e6ced4e2
Ruslan Igorevich <ruslan.igorevich@list.ru>
2019-05-18 22:50:41 +0300

    add vuex

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,15 @@
   <div class="about pt-5">
     <v-layout align-center column justify-center>
       <h1 class="display-2 font-weight-thin mb-3">This is an about page</h1>
+      <div class="pt-2">
+        <v-btn fab dark color="teal lighten-2">
+          <v-icon dark>remove</v-icon>
+        </v-btn>
+
+        <v-btn fab dark color="red darken-2">
+          <v-icon dark>add</v-icon>
+        </v-btn>
+      </div>
     </v-layout>
     <Grid/>
   </div>
